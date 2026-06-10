8cf9a79e221b8f0304a27e0ae0660da724d37c00
OberonGG <mrfajri90@gmail.com>
2026-06-10 13:05:23 +0800

    Update clean.lua

--- clean.lua
+++ clean.lua
@@ -4,7 +4,7 @@ local function waitForReady()
     while not CoreGui:FindFirstChild("MengHubGui") 
     or not CoreGui:FindFirstChild("ToggleUIButton")
     or not CoreGui:FindFirstChild("NotifyGui") do
-        task.wait(2)
+        task.wait(10)
     end
 end
 
@@ -22,7 +22,7 @@ local function waitForFullyLoaded()
                 lastCount = currentCount
             end
         end
-        task.wait(2)
+        task.wait(10)
     end
 end
 
