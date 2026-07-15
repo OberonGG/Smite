9ddfde33448353a42925970ea1f0e21708f1eeef
OberonGG <mrfajri90@gmail.com>
2026-07-16 01:12:11 +0800

    Update newtest.lua

--- newtest.lua
+++ newtest.lua
@@ -416,17 +416,6 @@ end
 
 -- AUTO CLOSE DELTA
 for _, gui in ipairs(CoreGui:GetChildren()) do
-	if gui:IsA("ScreenGui") then
-		local isWeird = false
-		local name = gui.Name
-		for i = 1, #name do
-			local c = name:sub(i, i)
-			if not c:match("[%w_]") then
-				isWeird = true
-				break
-			end
-		end
-		if isWeird then
 			local hasConsole = false
 			for _, obj in ipairs(gui:GetDescendants()) do
 				if obj:IsA("GuiObject") and string.find(obj.Name, "Console") then
@@ -440,9 +429,5 @@ for _, gui in ipairs(CoreGui:GetChildren()) do
 						obj:Destroy()
 					end
 				end
-			else
-				gui:Destroy()
 			end
 		end
-	end
-end
